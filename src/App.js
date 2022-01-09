5bbf34902a6567101607e3f3fa19491167146116
climb719 <mary.oldweiler@gmail.com>
2022-01-09 14:29:22 -0500

    Nav bar content, add router, css for nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
 import ProjectIndex from './containers/ProjectIndex';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
+   
     <div className="App">
+    <Nav />
     <header className="App-header">
        <h1>Portfolio</h1>
     </header> 
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Nav = props => {
+    return (
+     <div className="nav-bar">
+     <ul>
+     <li><Link to="/about"><div className='link'>About</div></Link></li>
+      <li><Link to="/projects"><div className='link'>Projects</div></Link></li>
+      <li><Link to="/blog"><div className='link'>Blog</div></Link></li>
+      <li><Link to="/skills"><div className='link'>Skills</div></Link></li>
+      <li><Link to="/contact"><div className='link'>Contact</div></Link></li>
+    </ul>
+
+      </div>
+      )
+    }
+    
+export default Nav;
