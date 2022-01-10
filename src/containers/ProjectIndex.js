1762df04c682e48bea8aa343a73375746a5a918c
climb719 <mary.oldweiler@gmail.com>
2022-01-10 12:36:35 -0500

    fetching projects from backend

--- src/containers/ProjectIndex.js
+++ src/containers/ProjectIndex.js
@@ -1,5 +1,31 @@
-function ProjectIndex(props) {
-    return <h1 className="page-heading">Projects!</h1>
+import { Component } from 'react'
+
+class ProjectIndex extends Component {
+
+    state = {
+       projects: []
+    }
+
+    componentDidMount() {
+        const url ="http://localhost:3000/projects"
+        fetch(url)
+        .then(resp => resp.json())
+        .then(data=> {
+            //console.log(data)
+            this.setState({
+                projects: data
+            })
+        })
+    }
+
+
+    render() {
+    return ( 
+    <>
+    <h1 className="page-heading">Projects!</h1>
+    </>
+    )
+    }
 }
 
 export default ProjectIndex
